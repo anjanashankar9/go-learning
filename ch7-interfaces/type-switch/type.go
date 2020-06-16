1abf3371db1f76f4020a849d576469deff90b78b
Anjana Shankar <anjanashankar9@gmail.com>
2020-06-16 23:25:23 +0530

    Type Switch interface

---
+++ ch7-interfaces/type-switch/type.go
@@ -0,0 +1,27 @@
+package main
+
+/*
+A type switch enables a multi-way branch based on the interface value's dynamic
+type. The nil case matches if x == nil, and the default case matches if no
+other case does. Example:
+	switch x.(type) {
+		case nil:      // ...
+		case int, uint // ...
+		case bool:     // ...
+		case string:   // ...
+		default:       // ...
+	}
+
+As with an ordinary switch statement, cases are considered in order, and when a match is found, the case's body is executed.
+Case order becomes significant when one or more case types are interfaces, since there is a possibility of two cases matching.
+
+Sometimes, the logic for a case needs access to the value extracted by the type assertion.
+	switch x := x.(type) { ... }
+
+Like a switch statement, a type switch implicitly creates a lexical block, so the dec- laration of the new variable called x does not conflict with a variable x in an outer block. Each case also implicitly creates a separate lexical block.
+
+*/
+
+func main() {
+
+}
